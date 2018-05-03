c93791fea3b7a0de5c47cf1167dbbf0e080c0a10
GRADID <gradidvorgen@yahoo.com>
2018-05-03 18:43:02 +0300

    Array methods homework has been done

--- src/arrayMethods.js
+++ src/arrayMethods.js
@@ -1,5 +1,42 @@
-window.every = function () {};
-window.some = function () {};
-window.forEach = function () {};
-window.filter = function () {};
-window.map = function () {};
+window.every = function (array, callback) {
+    var result = true;
+    for (var i = 0; i < array.length; i++) {
+       if (!callback(array[i], i, array)) {
+            result = false;
+           break;
+       }
+    }
+    return result;
+};
+window.some = function (array, callback) {
+    var result = false;
+    for (var i = 0; i < array.length; i++) {
+       if (callback(array[i], i, array)) {
+            result = true;
+           break;
+       }
+    }
+    return result;
+};
+window.forEach = function (array, callback) {
+    var result;
+    for (var i = 0; i < array.length; i++) {
+        callback(array[i], i, array);
+    }
+};
+window.filter = function (array, callback) {
+    var filteredArray = [];
+    for (var i = 0; i < array.length; i++) {
+        if (callback(array[i], i, array)) {
+            filteredArray.push(array[i]);
+        }
+    }
+    return filteredArray;
+};
+window.map = function (array, callback) {
+    var mapedArray = [];
+    for (var i = 0; i < array.length; i++) {
+        mapedArray[i] = (callback(array[i], i, array));
+    }
+    return mapedArray;
+};
