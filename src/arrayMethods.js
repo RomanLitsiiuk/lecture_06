a4e8aa46c6348f1e27e18aa603f0bcb02ba326d5
GRADID <gradidvorgen@yahoo.com>
2018-05-03 18:54:37 +0300

    Spaces were fixed

--- src/arrayMethods.js
+++ src/arrayMethods.js
@@ -1,42 +1,41 @@
 window.every = function (array, callback) {
-    var result = true;
-    for (var i = 0; i < array.length; i++) {
-       if (!callback(array[i], i, array)) {
-            result = false;
-           break;
-       }
+  var result = true;
+  for (var i = 0; i < array.length; i++) {
+    if (!callback(array[i], i, array)) {
+      result = false;
+      break;
     }
-    return result;
+  }
+  return result;
 };
 window.some = function (array, callback) {
-    var result = false;
-    for (var i = 0; i < array.length; i++) {
-       if (callback(array[i], i, array)) {
-            result = true;
-           break;
-       }
+  var result = false;
+  for (var i = 0; i < array.length; i++) {
+    if (callback(array[i], i, array)) {
+      result = true;
+      break;
     }
-    return result;
+  }
+  return result;
 };
 window.forEach = function (array, callback) {
-    var result;
-    for (var i = 0; i < array.length; i++) {
-        callback(array[i], i, array);
-    }
+  for (var i = 0; i < array.length; i++) {
+    callback(array[i], i, array);
+  }
 };
 window.filter = function (array, callback) {
-    var filteredArray = [];
-    for (var i = 0; i < array.length; i++) {
-        if (callback(array[i], i, array)) {
-            filteredArray.push(array[i]);
-        }
+  var filteredArray = [];
+  for (var i = 0; i < array.length; i++) {
+    if (callback(array[i], i, array)) {
+      filteredArray.push(array[i]);
     }
-    return filteredArray;
+  }
+  return filteredArray;
 };
 window.map = function (array, callback) {
-    var mapedArray = [];
-    for (var i = 0; i < array.length; i++) {
-        mapedArray[i] = (callback(array[i], i, array));
-    }
-    return mapedArray;
+  var mapedArray = [];
+  for (var i = 0; i < array.length; i++) {
+    mapedArray[i] = (callback(array[i], i, array));
+  }
+  return mapedArray;
 };
